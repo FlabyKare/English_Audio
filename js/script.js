5a6a1267cdde8a5b89cad0eb58b0739d069ef62b
FlabyKare <103658067+FlabyKare@users.noreply.github.com>
2022-10-28 20:40:20 +0300

    added search input

--- js/script.js
+++ js/script.js
@@ -12,3 +12,34 @@ $(document).ready(function () {
       return false;
    });
 });
+
+
+
+
+let cards = document.querySelectorAll('.audio_cont')
+    
+function liveSearch() {
+    let search_query = document.getElementById("searchbox").value;
+    
+    //Use innerText if all contents are visible
+    //Use textContent for including hidden elements
+    for (var i = 0; i < cards.length; i++) {
+        if(cards[i].textContent.toLowerCase()
+                .includes(search_query.toLowerCase())) {
+            cards[i].classList.remove("is-hidden");
+        } else {
+            cards[i].classList.add("is-hidden");
+        }
+    }
+}
+
+//A little delay
+let typingTimer;               
+let typeInterval = 0;  
+let searchInput = document.getElementById('searchbox');
+
+searchInput.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(liveSearch, typeInterval);
+});
+
